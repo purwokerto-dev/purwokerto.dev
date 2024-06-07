3a177ee44aa897d27b9469d4b281721da6d1953e
Chopper <riskidrian1@gmail.com>
2024-06-07 20:37:54 +0700

    efactor : nextauth pages to app with session lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import prisma from "@/lib/db";
+import Link from "next/link";
 
 export default async function Home() {
   const users = await prisma.user.findMany();
+
   return (
     <main>
       {users.map((user, index) => (
@@ -11,6 +13,9 @@ export default async function Home() {
           <li>email: {user.email}</li>
         </ul>
       ))}
+      <br />
+
+      <Link href="example-login">Example Login</Link>
     </main>
   );
 }
