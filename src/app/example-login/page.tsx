29f3f96237addf0dc63d0cab1542d431ba7889ef
Chopper <riskidrian1@gmail.com>
2024-06-07 20:30:30 +0700

    efactor : nextauth pages to app

---
+++ src/app/example-login/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function LoginPage() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div>
+        <p>Welcome, {session.user?.name}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => signIn("google")}>Sign in with Google</button>
+    </div>
+  );
+}
