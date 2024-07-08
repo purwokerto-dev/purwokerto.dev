128992414439e4c05983543d8b3809cf40d68519
Chopper <riskidrian1@gmail.com>
2024-07-08 14:39:11 +0700

    chore: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [""],
+    domains: ["example.com"],
     deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96],
     loader: "default",
