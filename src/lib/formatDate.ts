5c059fb97467f6ae47945057ca39260544f097b6
Chopper <riskidrian1@gmail.com>
2024-07-17 15:08:54 +0700

    feat: format date

---
+++ src/lib/formatDate.ts
@@ -0,0 +1,15 @@
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const formattedDate = date.toLocaleDateString(undefined, options);
+
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `${formattedDate} - ${hours}:${minutes}`;
+}
