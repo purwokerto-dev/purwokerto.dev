17f981039396e2d171eb2580e8ca173b57a90d7d
aditya <aditya.roshadi@3equals.io>
2024-07-18 15:07:50 +0700

    more blog doc

--- src/app/api/blogs/[id]/route.ts
+++ src/app/api/blogs/[id]/route.ts
@@ -6,6 +6,24 @@ import { authOptions } from "@/lib/auth";
 
 const prisma = new PrismaClient();
 
+/**
+ * @swagger
+ * /api/blogs/{id}:
+ *   get:
+ *     tags:
+ *       - Blog
+ *     description: Returns details of a blog
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The blog's ID
+ *     responses:
+ *       200:
+ *         description: Returns details of a blog
+ */
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
     // Fetch a single blog
     try {
@@ -18,6 +36,45 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
     }
 }
 
+/**
+ * @swagger
+ * /api/blogs/{id}:
+ *   put:
+ *     tags:
+ *       - Blog
+ *     description: Modify details of a blog [Requires admin privilege]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The blog's ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: Title of the blog
+ *               body:
+ *                 type: string
+ *                 description: Body of the blog
+ *               draft:
+ *                 type: number
+ *                 description: 1 for draft, 2 for ready, 3 for published
+ *               tags:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 description: array of tag ids (array of string)
+ *     responses:
+ *       200:
+ *         description: Returns details of a modified blog
+ */
 export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
     // Check the session
     const session: any = await getServerSession(authOptions);
@@ -59,6 +116,24 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
     }
 }
 
+/**
+ * @swagger
+ * /api/blogs/{id}:
+ *   delete:
+ *     tags:
+ *       - Blog
+ *     description: Delete a blog [Requires admin privilege]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The blog's ID
+ *     responses:
+ *       200:
+ *         description: Returns details of a deleted blog
+ */
 export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
     const session: any = await getServerSession(authOptions);
     if (!session || !session.user.isAdmin) {
