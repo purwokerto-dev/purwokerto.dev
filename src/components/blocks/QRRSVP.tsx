38913d4466ca96252655f0088ea5fafa2c3da42f
Chopper <riskidrian1@gmail.com>
2024-07-24 23:22:59 +0700

    fix: undefined window origin

--- src/components/blocks/QRRSVP.tsx
+++ src/components/blocks/QRRSVP.tsx
@@ -1,8 +1,17 @@
 "use client";
+import { useEffect, useState } from "react";
 import QR from "../fragments/QR";
 
 const QRRSVP = ({ rsvplink }: { rsvplink: string }) => {
-  const baseUrl = window.location.origin;
+  const [baseUrl, setBaseUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setBaseUrl(window.location.origin);
+    }
+  }, []);
+
+  if (!baseUrl) return null;
 
   return <QR text={`${baseUrl}${rsvplink}`} />;
 };
