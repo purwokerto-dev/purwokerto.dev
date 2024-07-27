9d0d0b8bbce5eed00eb5ce7b3cf2b08039f1d7e3
Chopper <riskidrian1@gmail.com>
2024-07-27 09:20:59 +0700

    chore: select reference event badge

--- src/components/blocks/form-create-event-badge.tsx
+++ src/components/blocks/form-create-event-badge.tsx
@@ -1,25 +1,69 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { Input } from "../fragments/input";
 import Button from "../fragments/button";
 import { useToast } from "../fragments/use-toast";
 import { useRouter } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../fragments/select";
 
 export function FormCreateEventBadge() {
   const { toast } = useToast();
   const navigate = useRouter();
+  const [dropdownList, setDropdownList] = useState({
+    events: [],
+    badges: [],
+    users: [],
+  });
+
   const [eventBadgeBody, setEventBadgeBody] = useState({
     eventId: "",
     badgeId: "",
     userId: "",
   });
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let { name, value } = e.target;
 
-    setEventBadgeBody((prev) => ({ ...prev, [name]: value }));
-  };
+  async function getEvents() {
+    try {
+      const { data } = await axios.get("/api/events?limit=100&open=true");
+      setDropdownList((prev) => ({ ...prev, events: data }));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  async function getBadges() {
+    try {
+      const { data } = await axios.get("/api/badges");
+
+      setDropdownList((prev) => ({ ...prev, badges: data }));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  async function getUsers() {
+    try {
+      const { data } = await axios.get("/api/users");
+      setDropdownList((prev) => ({ ...prev, users: data }));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    getBadges();
+    getEvents();
+    getUsers();
+  }, []);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -63,48 +107,84 @@ export function FormCreateEventBadge() {
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <div>
           <label htmlFor="eventId" className="font-semibold">
-            Event Id <span className="text-red-500">*</span>
+            Event <span className="text-red-500">*</span>
           </label>
-          <Input
-            required
-            id="eventId"
-            type="text"
-            placeholder="eventId"
+          <Select
             name="eventId"
-            className=""
             value={eventBadgeBody.eventId}
-            onChange={handleChange}
-          />
+            onValueChange={(value) =>
+              setEventBadgeBody((prev) => ({ ...prev, eventId: value }))
+            }>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select Event" />
+            </SelectTrigger>
+            <SelectContent className="bg-white dark:bg-primary">
+              <SelectGroup>
+                {dropdownList.events?.map((event: any) => (
+                  <SelectItem
+                    key={event.id}
+                    className="cursor-pointer"
+                    value={event.id}>
+                    {event.title}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
         <div>
           <label htmlFor="badgeId" className="font-semibold">
-            Badge Id <span className="text-red-500">*</span>
+            Badge <span className="text-red-500">*</span>
           </label>
-          <Input
-            required
-            id="badgeId"
-            type="text"
-            placeholder="badgeId"
+          <Select
             name="badgeId"
-            className=""
             value={eventBadgeBody.badgeId}
-            onChange={handleChange}
-          />
+            onValueChange={(value) =>
+              setEventBadgeBody((prev) => ({ ...prev, badgeId: value }))
+            }>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select Badge" />
+            </SelectTrigger>
+            <SelectContent className="bg-white dark:bg-primary">
+              <SelectGroup>
+                {dropdownList.badges?.map((badge: any) => (
+                  <SelectItem
+                    key={badge.id}
+                    className="cursor-pointer"
+                    value={badge.id}>
+                    {badge.title}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
         <div>
           <label htmlFor="userId" className="font-semibold">
             Speaker <span className="text-red-500">*</span>
           </label>
-          <Input
-            required
-            id="userId"
-            type="text"
-            placeholder="userId"
+          <Select
             name="userId"
-            className=""
             value={eventBadgeBody.userId}
-            onChange={handleChange}
-          />
+            onValueChange={(value) =>
+              setEventBadgeBody((prev) => ({ ...prev, userId: value }))
+            }>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select Speaker" />
+            </SelectTrigger>
+            <SelectContent className="bg-white dark:bg-primary">
+              <SelectGroup>
+                {dropdownList.users?.map((user: any) => (
+                  <SelectItem
+                    key={user.id}
+                    className="cursor-pointer"
+                    value={user.id}>
+                    {user.name}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
         <div>
           <Button type="submit" text="Submit" variant="bg" />
